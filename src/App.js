c693f2e8f3a3bda65ed326661497e808edf44599
jenna-graham <jclayto2@gmail.com>
2022-06-15 17:23:37 -0700

    initiated state for book array and adding new book objects, wrote submit book function in app.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Book from './Book.js';
+import BookForm from './BookForm.js';
 
 function App() {
+  const [bookTitle, setTitle] = useState('');
+  const [bookDate, setDate] = useState('');
+  const [bookAuthor, setAuthor] = useState('');
+  const [bookColor, setColor] = useState('');
+  const [allBooks, SetBooks] = useState([]);
+  const [visibleBooks, setBooks] = useState('');
+  const [filterString, setFilterString] = useState('');
+
+  function submitBook(e) {
+    e.preventDefault();
+    const newBook = {
+      title: bookTitle,
+      author: bookAuthor,
+      date: bookDate,
+      color: bookColor,
+    };
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BookForm 
+        submitBook={submitBook}
+        bookTitle={bookTitle}
+        setTitle={setTitle}
+        bookAuthor={bookAuthor}
+        setAuthor={setAuthor}
+        bookDate={bookDate}
+        setDate={setDate}
+        bookColor={bookColor}
+        setColor={setColor}
+      />
+      <Book book={{
+        title: bookTitle,
+        date: bookDate,
+        author: bookAuthor,
+        color: bookColor,
+      }}/>
     </div>
+   
+   
   );
 }
 
